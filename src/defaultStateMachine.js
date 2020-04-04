782a478f8de223842d690666997f11ee643e9e82
Marcus Sonntag <sonntag.marcus@yahoo.com>
2020-04-04 16:32:41 +0200

    Improve default state-machine

--- src/defaultStateMachine.js
+++ src/defaultStateMachine.js
@@ -1,47 +1,81 @@
+// First define names guards, actions, ...
+
+/**
+ * Guards
+ */
 const maxValueReached = (context, event) => {
   return context.counter >= 10;
 };
 
+/**
+ * Actions
+ */
 const incrementCounter = (context, event) => {
   context.counter += 1;
-}
+};
 
 const resetCounter = (context, event) => {
+  // Can send log messages via
+  //  - node.log
+  //  - node.warn
+  //  - node.error
   node.warn("RESET");
-  context.counter = 0;
-}
 
+  context.counter = 0;
+
+  // Can send messages to the second outport
+  // Specify an array to send multiple messages
+  // at once
+  //  - node.send(msg)
+  node.send({ payload: "resetCounter" });
+};
+
+/**
+ * Activities
+ */
+const doStuff = () => {
+  debugger;
+  const interval = setInterval(() => node.warn('BEEP'), 1000);
+  return () => clearInterval(interval);
+};
+
+/***************************
+ * Main machine definition * 
+ ***************************/
 return {
   machine: {
     context: {
-        counter: 0
+      counter: 0
     },
     initial: 'count',
     states: {
       count: {
-          on: {
-              '': { target: 'reset', cond: 'maxValueReached' }
-          },
-          after: {
-            500: { target: 'count', actions: 'incrementCounter' }
-          }
+        on: {
+          '': { target: 'reset', cond: 'maxValueReached' }
+        },
+        after: {
+          500: { target: 'count', actions: 'incrementCounter' }
+        }
       },
       reset: {
-          entry: 'resetCounter',
-          after: {
-              '1000': { target: 'count' }
-          }
+        entry: 'resetCounter',
+        after: {
+          '5000': { target: 'count' }
+        },
+        activities: 'doStuff'
       }
     }
   },
   // Configuration containing guards, actions, activities, ...
+  // see above
   config: {
-      guards: { maxValueReached },
-      actions: { incrementCounter, resetCounter }
+    guards: { maxValueReached },
+    actions: { incrementCounter, resetCounter },
+    activities: { doStuff }
   },
   // Define listeners (can be an array of functions)
-  //    Functions get called on every update
-  listeners: (data) => { 
-      node.warn(data.state + ":" + data.context.counter); 
+  //    Functions get called on every state/context update
+  listeners: (data) => {
+    node.warn(data.state + ":" + data.context.counter);
   }
-}
+};
