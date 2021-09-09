c8e2d6af7ba1242b02214389e62a963faf9fa6c1
Marcus Sonntag <sonntag.marcus@yahoo.com>
2021-09-09 08:24:04 +0200

    Fixed display of state actions due to change in xstate

---
+++ .changeset/late-ears-rule.md
@@ -0,0 +1,5 @@
+---
+"node-red-contrib-xstate-machine": patch
+---
+
+Fixed display of state actions due to change in xstate
--- src/xstate-smcat.js
+++ src/xstate-smcat.js
@@ -15,8 +15,8 @@ function getStateAction(actionTrigger) {
     return actionTrigger
         .filter( e => !e.hasOwnProperty('id') || !e.id.startsWith("xstate.after(") )
         .filter( e => !e.hasOwnProperty('sendId') || !e.sendId.startsWith("xstate.after(") )
-        .filter( e => ( e.toString() ? true : false) )
-        .map( e => (e.id ? e.id : e.toString()).replace(/['";,{}\[\]]\s/sgi,'') + '()' )
+        .filter( e => ( e.type ? true : false) )
+        .map( e => (e.id ? e.id : e.type).replace(/['";,{}\[\]]\s/sgi,'') + '()' )
         .join(' ');
 }
 
